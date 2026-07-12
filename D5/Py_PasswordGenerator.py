39b27f0c830785d1aa7295200c45a31677957c4d
Ayush Gupta <ayushgupta7170@gmail.com>
2026-07-12 10:55:20 +0530

    changes

--- D5/Py_PasswordGenerator.py
+++ D5/Py_PasswordGenerator.py
@@ -2,7 +2,7 @@
 
 import random
 
-# The characters we can pick from
+
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
@@ -16,18 +16,39 @@ lr=int(input("How many letters do you want in your password? "))
 sy=int(input("How many symbols do you want in your password? "))
 nr=int(input("How many numbers do you want in your password? "))
 
-password=[]
+
+password=""
 password_final=""
 
 for i in range(1,lr+1):
-    password.append(random.choice(letters))
+    password+=random.choice(letters)
 
 for i in range(1,sy+1):
-    password.append(random.choice(symbols))
+    password+=random.choice(symbols)
 
 for i in range(1,nr+1):
-    password.append(random.choice(numbers))
+   password+=random.choice(numbers)
 
-password_final=password_final.join(password)
+for i in range(0,len(password)):
+   password_final+=random.choice(password)
 
 print(f"Your password is: {password_final}")
+
+# Another method to do it using Lists:-
+
+password_list = []
+
+for i in range(0, lr):
+    password_list.append(random.choice(letters))
+
+for i in range(0, sy):
+    password_list.append(random.choice(symbols))
+
+for i in range(0, nr):
+    password_list.append(random.choice(numbers))
+
+random.shuffle(password_list)
+
+password_new = "".join(password_list)
+
+print(f"Your password is: {password_new}")
